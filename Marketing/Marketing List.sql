12e81ed66291524a9bae9aa57c148e26b94c1089
Aboulfazl Kamrani <129895774+akamranii@users.noreply.github.com>
2023-11-27 03:45:49 -0800

    Sales List

---
+++ Marketing/Marketing List.sql
@@ -0,0 +1,7 @@
+﻿UPDATE laptop SET memory = '1t'
+UPDATE laptop SET ram = '32'
+DELETE FROM laptop WHERE Id = 5
+SELECT * FROM laptop
+DELETE FROM laptop WHERE screen = 'fhd'
+INSERT INTO laptop (Id, model, color, memory, ram, chipset, screen, material, cpu, battry, price)
+VALUES ('7', 'acer', 'black', '1t', '32', 'h610', 'oled', 'metal', 'i7 g13', '10000', '2500')
---
+++ Marketing/Marketing list2.sql
@@ -0,0 +1,8 @@
+﻿SELECT * FROM phone;
+SELECT * FROM phone where color = 'black';
+UPDATE phone SET price = '1400';
+UPDATE phone SET battry = '5000';
+SELECT * FROM phone where chipset = 'kirin'
+SELECT * FROM phone where screen = 'oled' and cpu = 'adreno 600'
+DELETE FROM phone WHERE Id = 2
+DELETE FROM phone WHERE memory = '128'
